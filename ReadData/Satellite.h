cb259fe1219a2ce9d23ccacec9ac0dad67c3ed3d
clynae2 <48706019+Lyra126@users.noreply.github.com>
2023-03-01 16:21:58 -0500

    Add files via upload

--- ReadData/Satellite.h
+++ ReadData/Satellite.h
@@ -1,17 +1,62 @@
 //
-// Created by Rebecca Diaz on 2/20/23.
+// Created by Clarissa Mac on 2/28/23.
 //
 
-#pragma once
+#ifndef TESTSGP4_SATELLITE_H
+#define TESTSGP4_SATELLITE_H
+
 #include "libsgp4/Tle.h"
 
-
 class Satellite {
-    libsgp4::Tle tle;
-public:
-    Satellite(std::string line1, std::string line2);
+    private:
+        TLE tle;
+        //variable to help rank satellites priority
+        int ranking;
+        float durationAccess;
 
-    void printName();
+        //in addition to TLE variables, need variables detailing when and were the antenna should point
+
+        //function to calculate - Where and when the antenna should point over a 7-day period (coordinates,
+        // angles as it changes over a 7-day period, take into account velocity of satellite moving)
+        string calculateSat(Satellite sat){
+            /* We are working with LEO satellites...
+             *
+             * important values to consider:
+             * - mean motion
+             * - epoch year and julian day fraction
+             * - eccentricity
+             * - inclination
+             * - right ascension of the ascension mode
+             * - argument of perigee
+             *  -revolution number at epoch and check sum
+             *
+             * Finding Coordinates:
+             * Orbital Formula: r = (h2/μ)/(1 + e cos θ)
+             * TLE converted to code: https://kaitlyn.guru/projects/two-line-elements-tle/
+             *
+             * Duration of Access: minimum (1-20 minutes) 7-15 is probably best
+             * https://www.mathworks.com/help/satcom/ug/satellite-constellation-access-to-groundstation.html
+             *
+             * Aerospace team will determine ground station location, so right now focus on how you will calculate
+             * where and when antenna will point, no need to fully implement and test, just brainstorm how your code
+             * will be
+             *
+             * !!Use SPG4 reference sheet and look through libsgp4 library to find any functions that might help
+             */
+        }
+    public:
+        //constructor
+        Satellite(TLE tle){
+            this->tle = tle;
+        }
+
+        void printName();
+
+        void addRanking(int n){
+            ranking = n;
+        }
+
 
 };
 
+#endif //TESTSGP4_SATELLITE_H
