d4d2e93474a04543f207a84e2d299b4f82ae6ff8
clynae2 <48706019+Lyra126@users.noreply.github.com>
2023-03-06 20:36:26 -0500

    Update Satellite.h

--- ReadData/Satellite.h
+++ ReadData/Satellite.h
@@ -19,19 +19,24 @@ class Satellite {
     private:
         libsgp4::Tle tle;
         //duration that the satellite will communicate with the ground station
-        libsgp4::SGP4 duration;
+        //libsgp4::SGP4 duration;
+        //start time?
         libsgp4::DateTime dt;
         libsgp4::Eci eci;
         libsgp4::CoordTopocentric topo;
         libsgp4::CoordGeodetic geo;
         libsgp4::Observer obs;
         libsgp4::SGP4 sgp4;
+        int rank;
 
     public:
         //constructor
-        Satellite(libsgp4::TLE tle){
-            this->tle = tle;
-            sgp4(tle);
+        //initialized some class object variables right when the constructor is built b/c most of them have no default constructor
+        //change obs and eci inputs later as needed
+        Satellite(libsgp4::Tle tle1): tle(tle1), sgp4(tle), obs(29,82,1), eci(dt, 1, 1, 1){
+            //this->tle = tle1;
+            //sgp4(tle);
+            assignRank();
         }
 
         //function to calculate - Where and when the antenna should point over a 7-day period (coordinates,
@@ -46,10 +51,25 @@ class Satellite {
             geo = eci.ToGeodetic();
         }
 
-        float calculateDistance(){
+        float calculateDistance() {
+            //need to code calculation for distance between ground station and satellite
+            //ground station is 29°38'53.1"N 82°20'51.1"W on top of Weimer Hall
+            //angle = arccos(point1 * point2)
+            //distance = angle * pi * radius
+        }
+
+        void assignRank(){
+            //need to code ranking system here (Aerospace team will hopefully provide this)
+            //assign variable rank with ranking
 
         }
 
+        void ToString(){
+            //need to figure out what output needs to be outputted for each satellite in the 7-day schedule
+            cout << eci << endl;
+            cout << topo << endl;
+            cout << geo << endl;
+        }
 };
 
 #endif //TESTSGP4_SATELLITE_H
