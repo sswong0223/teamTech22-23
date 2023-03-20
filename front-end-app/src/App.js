735b69d034f8c3fc218fa0b3a0080d8af93cdd49
chos906 <80611197+chos906@users.noreply.github.com>
2023-03-20 18:38:44 -0400

    Weekly Calendar

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,63 +1,30 @@
-// combine caci and swe logo into one image
-// import logo from './images/sweCaciLogo.png';
-import Navbar from './components/Navbar';
-// to install calendar use npm install react-big-calendar react-datepicker date-fns
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import './App.css';
+import "./index.css";
+import { useState } from "react";
+import Calendar from "./Calendar";
+import Details from "./Details";
 
-const locales = {
-  "en-US": require("date-fns/locale/en-US")
-};
+export default function App() {
+  const [showDetails, setShowDetails] = useState(false);
+  const [data, setData] = useState(null);
 
-const localizer = dateFnsLocalizer ({
-  format,
-  parse,
-  startOfWeek,
-  getDay, 
-  locales
-});
-// array of objects, can hard code for now, then implement back end somehow
-const events = [
-  {
-    title: "Satellite One",
-    allDay: true,
-    start: new Date(2023, 3, 2),
-    end: new Date(2023, 3, 2)
-  },
-  {
-    title: "Satellite Two",
-    start: new Date(2023, 3, 27),
-    end: new Date(2023, 3, 29)
-  },
-  {
-    title: "Satellite Three",
-    start: new Date(2023, 4, 1),
-    end: new Date(2023, 4, 6)
-  }
-];
+  const showDetailsHandle = (dayStr) => {
+    setData(dayStr);
+    setShowDetails(true);
+  };
 
-function App() {
-  
   return (
-    <div className = "App">
-      <Navbar/>
-        <Calendar 
-          localizer={localizer} 
-          events={events} 
-          startAccessor="start" 
-          endAccessor="end" 
-          style={{height: 500, marign: "50px"}} 
-        />
+    <div className="App">
+      <h1>Week View Calendar</h1>
+      <br />
+      <Calendar showDetailsHandle={showDetailsHandle} />
+      <br />
+      {showDetails && <Details data={data} />}
     </div>
   );
 }
 
-export default App;
+/**
+ * Follow this tutorial https://medium.com/@moodydev/create-a-custom-calendar-in-react-3df1bfd0b728
+ * and customised by TTNT
+ * date-fns doc: https://date-fns.org/v2.21.1/docs
+ */
