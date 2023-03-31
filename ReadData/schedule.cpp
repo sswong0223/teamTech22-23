102923d5efe8cd9aeecf5b463386b40eb54bdc1b
clynae2 <48706019+Lyra126@users.noreply.github.com>
2023-03-31 16:50:23 -0400

    Update schedule.cpp

--- ReadData/schedule.cpp
+++ ReadData/schedule.cpp
@@ -4,6 +4,7 @@
 #include "libsgp4/SGP4.h"
 #include "Satellite.h"
 #include "libsgp4/Tle.h"
+#include "libsgp4/DateTime.h"
 #include <vector>
 #include <string>
 #include <queue>
@@ -15,18 +16,25 @@ using namespace std;
 void addSatelliteToHeaps(vector<priority_queue<Satellite, vector<int>, greater<int>>>& heap, Satellite& sat); //might change to string if time is in string
 //operator overload to be able to compare times in satellite class, change data type of the things in priority_queue later
 int main() {
+    // DEFAULT DATE TIME
+    //needs to be military udt time
+
+    libsgp4::DateTime currentTime(DateTime::Now());
+
+    // Hard code test: delete later
+    // Name: STARLINK-4655
+
+    std::string line1 = "1 53777U 22111E   23090.20735931  .00040371  00000+0  25383-2 0  9997";
+    std::string line2 = "2 53777  53.2173  57.8182 0001266 122.7022 237.4093 15.08867645 30678";
+    libsgp4::Tle obj1(line1, line2);
+    Satellite satellite(obj1, currentTime);
 
     /*
-    // Hard code test: delete later
-    std::string line1 = "1 00900U 64063C   23046.50614910  .00001371  00000+0  14455-2 0  9991";
-    std::string line2 = "2 00900  90.1810  45.4651 0027461  15.1398 149.1107 13.74104475904037";
-    libsgp4::Tle obj1(line1, line2);
-    std::cout << obj1.Name() << std::endl;
-*/
     // Initialize variables for reading input file
     int numRanks = 8; //!!!change later!!!
     std::string buffer;
-    std::fstream input("active.txt"); // The file of satellites is found in the debug folder
+    //updated March 31,2022 1:48 pm est
+    std::fstream input("celestrakList.txt"); // The file of satellites is found in the debug folder
     string line1, line2;
 
     //Initialize variables for scheduler
@@ -36,24 +44,26 @@ int main() {
     vector<Satellite> schedule;
 
 
+
     // Read in data from active.txt file until you reach the end of the file
     while(getline(input, buffer)){
+
         //read TLE lines
         getline(input, line1);
         getline(input, line2);
 
-        line1.pop_back(); // Gets rid of carriage return character
-        line2.pop_back(); // Gets rid of carriage return character
+        //line1.pop_back(); // Gets rid of carriage return character
+        //line2.pop_back(); // Gets rid of carriage return character
         //create a TLE object with line 1 and line 2 as arguments
         libsgp4::Tle tle(line1, line2);
         //create a satellite object with the tle as an argument
-        Satellite satellite(tle);
+        Satellite satellite(tle, currentTime);
         if(satellite.isLEO())
         //add satellite object to a max heap prioritized by ranking
         satellites.push_back(satellite);
     }
     //satellites.at(0).printName();
-
+*/
     return 0;
 }
 
@@ -88,12 +98,21 @@ string printSchedule(vector<Satellite> schedule){
 void addSatelliteToHeaps(vector<priority_queue<Satellite, vector<Satellite>, greater<Satellite>>>& heap, Satellite& sat){
     //add more if statements if more ranks
     if(sat.getRank() == 1){
-        heap[0].push(sat); //push error here because greater::operator won't work correctly?
+        //heap[0].push(sat); //push error here because greater::operator won't work correctly?
     }
     else if(sat.getRank() == 2){
-        heap[1].push(sat);
+        //heap[1].push(sat);
     }
     else if(sat.getRank() == 3){
-        heap[2].push(sat);
+        //heap[2].push(sat);
     }
+}
+
+float radiansToDegrees(float radians){
+    float degrees ;
+
+    degrees = radians * 180 / M_PI ;
+
+    return degrees;
+
 }
