ba88265d194641b7784a935df0ff20c101ed14c0
clynae2 <48706019+Lyra126@users.noreply.github.com>
2023-04-10 19:16:58 -0400

    Update Satellite.h

--- ReadData/Satellite.h
+++ ReadData/Satellite.h
@@ -16,76 +16,77 @@
 using namespace std;
 
 class Satellite {
-    private:
-        libsgp4::Tle tle;
+private:
+    libsgp4::Tle tle;
 
-        //duration that the satellite will communicate with the ground station
-        // vector of start time end time pairs
-        std::vector<std::pair<libsgp4::DateTime, libsgp4::DateTime>> passes;
+    //duration that the satellite will communicate with the ground station
+    // vector of start time end time pairs
+    std::vector<std::pair<libsgp4::DateTime, libsgp4::DateTime>> passes;
 
+    libsgp4::DateTime startTime;
+    libsgp4::DateTime endTime;
+    libsgp4::DateTime dt;
+    libsgp4::Eci eci;
+    libsgp4::CoordTopocentric topo;
+    libsgp4::CoordGeodetic geo;
+    libsgp4::Observer obs;
+    libsgp4::SGP4 sgp4;
+    double maxElevation;
+    int rank;
 
-        libsgp4::DateTime dt;
-        libsgp4::Eci eci;
-        libsgp4::CoordTopocentric topo;
-        libsgp4::CoordGeodetic geo;
-        libsgp4::Observer obs;
-        libsgp4::SGP4 sgp4;
-        int rank;
-
-    public:
-        //constructor
-        //initialized some class object variables right when the constructor is built b/c most of them have no default constructor
-        //change obs and eci inputs later as needed
-        Satellite(libsgp4::Tle tle1, libsgp4::DateTime currentTime): tle(tle1), dt(currentTime), eci(dt, 1, 1, 1), obs(29,82,1), sgp4(tle){
-            //this->tle = tle1;
-            //sgp4(tle);
-            // OBS: 29, 82, 1
-            eci = sgp4.FindPosition(dt);
-            geo = eci.ToGeodetic();
-            assignRank();
-            // calculate passes
-        }
-
-        bool isLEO();
-
-        //function to calculate - Where and when the antenna should point over a 7-day period (coordinates,
-        // angles as it changes over a 7-day period, take into account velocity of satellite moving)
-        void calculatePos(float timePassed);
-
-        float calculateDistance();
-
-        void assignRank();
-
-
-        void toString();
-
-        int getRank(){
-            return rank;
-        }
-
-        libsgp4::DateTime getEarliestEndTime() const{
-            return passes.at(0).second;
-        }
-
-        //work on this after we know how endTime is formatted
-        bool operator> (const Satellite& rSat) const{
-            if(getEarliestEndTime().Compare(rSat.getEarliestEndTime()) == 1) //endTime > rSat.getEndTime()
-                return true;
-            else
-                return false;
-        }
-
-        void generatePasses();
-
-};
-
-class satComparator{
 public:
-    int operator()(const Satellite& lSat, const Satellite& rSat){
-        if(lSat.getEarliestEndTime().Compare(rSat.getEarliestEndTime()) == 1) //endTime > rSat.getEndTime()
+    //constructor
+    //initialized some class object variables right when the constructor is built b/c most of them have no default constructor
+    //change obs and eci inputs later as needed
+    Satellite(libsgp4::Tle tle1, libsgp4::DateTime currentTime): tle(tle1), dt(currentTime), eci(dt, 1, 1, 1), obs(29,82,1), sgp4(tle){
+        //this->tle = tle1;
+        //sgp4(tle);
+        // OBS: 29, 82, 1
+        maxElevation = 0;
+        eci = sgp4.FindPosition(dt);
+        geo = eci.ToGeodetic();
+        assignRank();
+        // calculate passes
+    }
+
+    bool isLEO();
+
+    //function to calculate - Where and when the antenna should point over a 7-day period (coordinates,
+    // angles as it changes over a 7-day period, take into account velocity of satellite moving)
+    void calculatePos(float timePassed);
+
+    float calculateDistance();
+
+    void assignRank();
+
+    libsgp4::DateTime getStartTime();
+
+    libsgp4::DateTime getEndTime();
+
+    libsgp4::DateTime setStartTime(libsgp4::DateTime st);
+
+    libsgp4::DateTime setEndTime(libsgp4::DateTime et);
+
+
+    string toString();
+
+    int getRank(){
+        return rank;
+    }
+
+    libsgp4::DateTime getEarliestEndTime() const{
+        return passes.at(0).second;
+    }
+
+    //work on this after we know how endTime is formatted
+    bool operator< (const Satellite& rSat) const{
+        if(getEarliestEndTime().Compare(rSat.getEarliestEndTime()) == -1) //endTime > rSat.getEndTime()
             return true;
         else
             return false;
-
     }
+
+    void generatePasses();
+
 };
+
