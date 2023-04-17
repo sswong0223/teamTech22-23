00f1684717942fcf4b0fe5b7c2935895183c72a0
clynae2 <48706019+Lyra126@users.noreply.github.com>
2023-04-17 19:21:05 -0400

    Update Satellite.h

--- ReadData/Satellite.h
+++ ReadData/Satellite.h
@@ -35,18 +35,14 @@ private:
     int rank;
     bool constructorSuccess;
 
+    // For the database
     std::string name;
     std::string startString;
     std::string endString;
 
 public:
     //constructor
-    //initialized some class object variables right when the constructor is built b/c most of them have no default constructor
-    //change obs and eci inputs later as needed
     Satellite(libsgp4::Tle tle1, libsgp4::DateTime currentTime): tle(tle1), dt(currentTime), eci(dt, 1, 1, 1), obs(29,82,1), sgp4(tle){
-        //this->tle = tle1;
-        //sgp4(tle);
-        // OBS: 29, 82, 1
         constructorSuccess = true;
         maxElevation = 0;
         try{
@@ -61,8 +57,6 @@ public:
 
     bool isLEO();
 
-    //function to calculate - Where and when the antenna should point over a 7-day period (coordinates,
-    // angles as it changes over a 7-day period, take into account velocity of satellite moving)
     void calculatePos(float timePassed);
 
     float calculateDistance();
@@ -83,25 +77,13 @@ public:
 
     libsgp4::DateTime setEndTime(libsgp4::DateTime et);
 
-
     void toString();
 
-    int getRank(){
-        return rank;
-    }
+    int getRank();
 
-    libsgp4::DateTime getEarliestEndTime() const{
-        return passes.at(0).second;
-    }
+    libsgp4::DateTime getEarliestEndTime() const;
 
-    //work on this after we know how endTime is formatted
-    bool operator< (const Satellite& rSat) const{
-        if(getEarliestEndTime().Compare(rSat.getEarliestEndTime()) == 1) //endTime > rSat.getEndTime()
-            return true;
-        else
-            return false;
-    }
-
-
+    // Overload < for Min Heap
+    bool operator< (const Satellite& rSat) const;
 };
 
