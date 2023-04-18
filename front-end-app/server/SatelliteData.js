66eddcd81e2e689d5b04afaf9ea47b7652ca87e0
kchen53 <kellychen@ufl.edu>
2023-04-18 01:48:15 -0400

    cleaned up a bit of code

--- front-end-app/server/SatelliteData.js
+++ front-end-app/server/SatelliteData.js
@@ -1,11 +1,7 @@
-import { config } from 'dotenv';
 import * as dotenv from 'dotenv'
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import connectDB from './mongodb/connect.js';
-import SatelliteModel from './models/Satellite.js';
 import satelliteRouter from './routes/Satellite.routes.js'
 
 dotenv.config();
@@ -32,29 +28,3 @@ const startServer = async () => {
 
 startServer();
 
-// app.get('/satellite', async (req, res) => {
-//     try {
-//         // Fetch all satellites from the database using the Mongoose model
-//         const satellites = await SatelliteModel.find();
-//         res.json(satellites);
-//       } catch (error) {
-//         // Handle any errors
-//         res.status(500).json({ error: 'Failed to fetch satellites' });
-//       }
-// });
-
-// app.post('/satellite', async (req, res) => {
-//     try {
-//         // Create a new satellite in the database using the Mongoose model
-//         const satellite = new SatelliteModel({ 
-//             name: req.body.name,
-//             startTime: req.body.startTime,
-//             endTime: req.body.endTime
-//         })
-//         await satellite.save();
-//         res.json(satellite);
-//       } catch (error) {
-//         // Handle any errors
-//         res.status(500).json({ error: 'Failed to create satellite' });
-//       }
-// });
