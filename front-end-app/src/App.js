96bad8df1f70bc8c8be5b7873ee909c2545dbbe7
kchen53 <kellychen@ufl.edu>
2023-04-28 10:27:38 -0400

    trying to pull

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -2,38 +2,30 @@ import "./index.css";
 import { useState } from "react";
 import Calendar from "./Calendar";
 import Details from "./Details";
-import date from "./datePicker"
-import datePicker from "./datePicker";
+import logo from "./images/sweCaciLogo.png";
+import showEvents from "./Event";
 
 export default function App() {
   const [showDetails, setShowDetails] = useState(false);
   const [data, setData] = useState(null);
-
   const showDetailsHandle = (dayStr) => {
     setData(dayStr);
     setShowDetails(true);
   };
 
-  function storeInput() {
-    var inputValue = document.getElementById("myInput").value;
-    // Do something with the input value stored in the variable
-  }
-
   return (
     <div className="App">
-      <h1>Week View Calendar</h1>
-      <input type="text" placeholder="mm/dd/yyyy" id="myInput"> </input>
-      <button onclick="storeInput()">Store Input</button>
+      <h1>CACI Satellite Schedule</h1>
       <br />
       <Calendar showDetailsHandle={showDetailsHandle} />
       <br />
-      {showDetails && <Details data={data} />}
+      <div className="Events">
+        {showDetails && <Details data={data} />}
+        {showEvents()}
+      </div>
+      <div className="Footer">
+        <img src={logo} width={'100'}/>
+      </div>
     </div>
   );
 }
-
-/**
- * Follow this tutorial https://medium.com/@moodydev/create-a-custom-calendar-in-react-3df1bfd0b728
- * and customised by TTNT
- * date-fns doc: https://date-fns.org/v2.21.1/docs
- */
---
+++ front-end-app/src/Event.js
@@ -0,0 +1,16 @@
+import "./index.css";
+import { useState } from "react";
+import {format} from "date-fns";
+const showEvents = () =>
+{
+    // create loop that searches for specific date
+    // day = format(day, "MM/dd/YYYY");
+    // call search function to search for specific date
+    return(
+        <div className="Events">
+            <h4> Satellite Information Down Here</h4>
+            <ul style={{textAlign:"left"}}>testing</ul>
+        </div>
+    )
+}
+export default showEvents
