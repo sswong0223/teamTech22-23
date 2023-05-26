f6fd4a4fa67326624e491ec143ce691ed6d2be7c
Sabrina Wong <79372599+sswong0223@users.noreply.github.com>
2023-05-25 20:46:19 -0400

    update frontend

--- front-end-app/src/Event.js
+++ front-end-app/src/Event.js
@@ -1,6 +1,23 @@
 import "./index.css";
 import React, { useState, useEffect } from "react";
-import { format } from "date-fns";
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function convertMilitaryStringToTime(militaryTimeString) {
+  const [hours, minutes, seconds] = militaryTimeString.split(':');
+  const currentDate = new Date();
+  currentDate.setHours(hours);
+  currentDate.setMinutes(minutes);
+  currentDate.setSeconds(seconds); // Set seconds to 0 for precision
+
+  return currentDate;
+}
 
 const ShowEvents = ( startDate ) => {
   // create loop that searches for specific date
@@ -15,17 +32,31 @@ const ShowEvents = ( startDate ) => {
 
       const response = await fetch('http://localhost:8080/api/satellite');
       const newSatellite = await response.json();
-     
 
+      newSatellite.sort((a,b) => {
+        
+        const milTimeA = convertMilitaryStringToTime(a.startTime.substring(11,19))
+        const milTimeB = convertMilitaryStringToTime(b.startTime.substring(11,19))
+        console.log("AsatID", a.name, "A Full", a.startTime, "milTimeA", milTimeA)
+        console.log("BsatID", b.name, "B Full", b.startTime, "milTimeB", milTimeB)
+        
+        return milTimeA - milTimeB;
+      });
+     
+      console.log( "response sat" ,newSatellite)
       const newSatelliteItems = [];
 
       // Iterate through the data and create a 2D array of satellite items
       newSatellite.forEach(item => {
-        const satelliteItem = [item.name, item.startTime, item.endTime];
-        const start = satelliteItem[1].substring(0,10)
-        const end = satelliteItem[2].substring(0,10)
+        // console.log(item)
 
-        const indexToUpdate = 8;
+        const satelliteItem = [item.name, item.startTime.substring(11,19), item.endTime.substring(11,19)];
+        const start = item.startTime.substring(0,10)
+        const end = item.endTime.substring(0,10)
+        // const start = satelliteItem[1].substring(0,10)
+        // const end = satelliteItem[2].substring(0,10)
+
+        // const indexToUpdate = 8;
         const dayUpdate = (parseInt(start.substring(8,10))+1).toString().padStart(2,'0');
         const nextDay = start.substring(0,8) + dayUpdate
         
@@ -33,19 +64,18 @@ const ShowEvents = ( startDate ) => {
         
         
         
-        if(start == startDate || end == nextDay) {
+        if(start === startDate || end === nextDay) {
           newSatelliteItems.push(satelliteItem);
-          // console.log("here");
-          console.log("today", start)
-          console.log("param" ,startDate)  
-          console.log("nextDay Day", dayUpdate)
-          console.log("Tommorrow", nextDay)
-          console.log("endDate", end);
+          // console.log("today", start)
+          // console.log("param" ,startDate)  
+          // console.log("nextDay Day", dayUpdate)
+          // console.log("Tommorrow", nextDay)
+          // console.log("endDate", end);
         }
         
       
       });
-    
+      console.log(newSatelliteItems);
       setSatellite(newSatelliteItems);
     }
 
@@ -53,25 +83,95 @@ const ShowEvents = ( startDate ) => {
   }, [startDate]);
 
   return (
-    <div className="Events">
+    <div className="Events"
+      style={{
+        backgroundColor: '#F5F5F5',
+      }}>
 
       {/* <h4> {format(new Date(startDate), "ccc dd MMM yy")} </h4> */}
-      <div>
-        <h1>Satellite Data</h1>
-        <ul>
-          <h3>
-          Number of Satellites today: {satelliteItems.length}
-          </h3>
+      <div 
+        className="head"
+        style={{
+          width: 'auto',
+          margin: 'auto',
+          backgroundColor: '#F5F5F5',
+        }}>
+        <h1
+          style={{
+            color: '#6a1fa4',
+            
+          }}>
+            Satellite Schedule for Ground Station at:
+        </h1>
+        <h2
+          style={{
+            color: '#6a1fa4',
+            
+          }}>
+            27 degrees north and 82 degrees west
+        </h2>
+        <h3>Number of Satellites: {satelliteItems.length} satellites</h3>
+        
+          
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650, backgroundColor: '#FFFFFF',border: '5px solid purple'} } aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" sx={{fontWeight: 'bold', fontSize: '130%'}}>SSC</TableCell>
+            <TableCell align="center" sx={{fontWeight: 'bold', fontSize: '130%'}}>
+                Start Time (UTC)
+            </TableCell>
+            <TableCell align="center" sx={{fontWeight: 'bold', fontSize: '130%'}}>
+              End Time (UTC)
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {satelliteItems.map((item, index) => (
-            <li key={index}>
-              Name: {item[0]},
-              Start Time: {item[1]}, 
-              End Time:{" "} {item[2]}
-            </li>
+            <TableRow
+              key={index}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center" component="th" scope="satelliteItems" sx={{fontSize: '90%'}}>
+                {item[0]}
+              </TableCell>
+              <TableCell align="center" sx={{fontSize: '90%'}}>
+                {item[1]}
+              </TableCell>
+              <TableCell align="center" sx={{fontSize: '90%'}}>
+                {item[2]}
+              </TableCell>
+
+            </TableRow>
           ))}
-        </ul>
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+
+
+
+
+
+          {/* <table>
+            <th>Name</th>
+            <th>Start Time</th>
+            <th>End Time</th>
+
+          {satelliteItems.map((item, index) => (
+            <tr key={index}>
+              <td> {item[0]} </td>
+              <td> {item[1].substring(11,19)} UTC </td>
+              <td> {item[2].substring(11,19)} UTC </td>
+            </tr>
+          ))}
+
+          </table> */}
+
       </div>
     </div>
+          
   );
 }
 
